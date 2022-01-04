4a715266dfaabb589437f8fdfd7b62458fed9238
Asher Gilani <gilaniasher@gmail.com>
2022-01-04 18:00:49 -0500

    Added CORS support on backend by adding in Chi

--- backend/go-graphql/go.mod
+++ backend/go-graphql/go.mod
@@ -5,7 +5,9 @@ go 1.17
 require (
 	github.com/99designs/gqlgen v0.14.0
 	github.com/aws/aws-sdk-go v1.42.27
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/google/uuid v1.3.0
+	github.com/rs/cors v1.8.2
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
 
--- backend/go-graphql/server.go
+++ backend/go-graphql/server.go
@@ -3,27 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gilaniasher/reddit-clone/backend/go-graphql/graph"
 	"github.com/gilaniasher/reddit-clone/backend/go-graphql/graph/generated"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	port := defaultPort
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
