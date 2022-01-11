1df1eca872155de50c22ae25c3b2767a75ea52bb
Asher Gilani <gilaniasher@gmail.com>
2022-01-11 17:01:54 -0500

    Added Cognito to serverless stack
    1. Also put dynamodb config and cognito config in separate files
    2. Update serverless stack name
    3. Commented out dynamodb steps in run-local script since it'll be in cloud now

--- run-local.sh
+++ run-local.sh
@@ -8,11 +8,11 @@ cmd4="cd backend/serverless && npm run start-db"
 gnome-terminal \
     --tab --title="React"         -e "bash -c '$cmd1'" \
     --tab --title="Go"            -e "bash -c '$cmd2'" \
-    --tab --title="View DynamoDB" -e "bash -c '$cmd3'" \
-    --tab --title="DynamoDB"      -e "bash -c '$cmd4'" \
+    #--tab --title="View DynamoDB" -e "bash -c '$cmd3'" \
+    #--tab --title="DynamoDB"      -e "bash -c '$cmd4'" \
     2> /dev/null
 
 echo "React: http://localhost:3000/"
 echo "Go Server: http://localhost:8080/"
-echo "DynamoDB Local: http://localhost:8000"
-echo "View DynamoDB: http://localhost:8001"
+#echo "DynamoDB Local: http://localhost:8000"
+#echo "View DynamoDB: http://localhost:8001"
